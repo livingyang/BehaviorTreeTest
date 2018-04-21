f69a60b492fb994cfcb80c995389fee115199c83
huanming li <lhm.cold@163.com>
2018-04-21 22:19:00 +0800

    更新 step函数

--- src/ReduxWorld.ts
+++ src/ReduxWorld.ts
@@ -1,6 +1,6 @@
 import * as redux from 'redux';
 
-enum PlayerState {
+export enum PlayerState {
     // 闲置
     Idle,
 
@@ -14,7 +14,7 @@ enum PlayerState {
     Make,
 }
 
-const Point = {
+export const Point = {
     x: 0,
     y: 0,
 }
@@ -31,27 +31,69 @@ export const Player = {
     }
 }
 
-export const World = {
-    players: [{...Player}]
+export function CreatePlayer() {
+    return {...Player};
 }
 
-const InitialState = {
+export const InitialWorldState = {
     players: [{...Player}]
 };
 
-function counter(state = InitialState, action: any) {
+// function WorldReducer(state = InitialWorldState, action: any) {
+//     switch (action.type) {
+//         case 'STEP':
+//             return state + 1;
+//         default:
+//             return state
+//     }
+// }
+
+export function AddPlayer(player = {...Player}) {
+    return {type: 'ADD_PLAYER', player};
+}
+
+export function CreateStep(time = 0) {
+    return {type: 'STEP', time};
+}
+
+function players(state = [], action: any) {
     switch (action.type) {
-        // case 'STEP':
-        //     return state + 1
-        // case 'DECREMENT':
-        //     return state - 1
-        default:
-            return state
+        case 'ADD_PLAYER': {
+            return [...state, action.player];
+        };
+        case 'STEP': {
+            return state.map((player) => {
+                return PlayerStep(player, action.world, action.time);
+            })
+        };
     }
+    return state;
 }
 
-function PlayerStep(state = InitialState, player = Player, step: 10) {
-
+function PlayerStep(player = Player, world = InitialWorldState, time = 10) {
+    switch(player.state) {
+        case PlayerState.Patrol: {
+            let distance = time * player.patrol.speed;
+            if (distance >= Math.abs(player.point.x - player.patrol.target.x) + Math.abs(player.point.y - player.patrol.target.y)) {
+                player.point = {...player.patrol.target};
+                player.patrol.target = {...player.patrol.nextTarget};
+                player.patrol.nextTarget = {...player.point};
+            }
+            else {
+                if (Math.abs(player.point.x - player.patrol.target.x) > distance) {
+                    player.point.x += (player.patrol.target.x > player.point.x) ? distance : -distance;
+                }
+                else {
+                    distance -= Math.abs(player.point.x - player.patrol.target.x);
+                    player.point.x = player.patrol.target.x;
+                    
+                    player.point.y += (player.patrol.target.y > player.point.y) ? distance : -distance;
+                }
+            }
+            return player;
+        };
+    }
+    return player;
 }
 
-redux.createStore(counter);
+export let ReduxWorld = redux.createStore(redux.combineReducers({players}));
--- tests/ReduxWorld.spec.ts
+++ tests/ReduxWorld.spec.ts
@@ -1,5 +1,37 @@
-import {Player} from '../src/ReduxWorld';
+import {Player, CreatePlayer, PlayerState, CreateStep, InitialWorldState, ReduxWorld, AddPlayer} from '../src/ReduxWorld';
 
-test('adds 1 + 2 to equal 3', () => {
-    console.log(Player)
+test('patrol', () => {
+    let player = CreatePlayer();
+    player.state = PlayerState.Patrol;
+    player.patrol.target.x = 10;
+    player.patrol.target.y = 10;
+    ReduxWorld.dispatch(AddPlayer(player));
+
+    let state = InitialWorldState;
+    state = ReduxWorld.getState() as any;
+
+    ReduxWorld.dispatch(CreateStep(10));
+    expect(state.players[0].point.x).toBe(0);
+
+    player.patrol.speed = 6;
+
+    ReduxWorld.dispatch(CreateStep(1));
+    expect(state.players[0].point.x).toBe(6);
+
+    ReduxWorld.dispatch(CreateStep(1));
+    expect(state.players[0].point.x).toBe(10);
+    expect(state.players[0].point.y).toBe(2);
+    
+    ReduxWorld.dispatch(CreateStep(2));
+    expect(state.players[0].point).toEqual({x: 10, y: 10});
+    expect(state.players[0].patrol.target).toEqual({x: 0, y: 0});
+    expect(state.players[0].patrol.nextTarget).toEqual({x: 10, y: 10});
+    
+    ReduxWorld.dispatch(CreateStep(2));
+    expect(state.players[0].point).toEqual({x: 0, y: 8});
+    
+    ReduxWorld.dispatch(CreateStep(2));
+    expect(state.players[0].point).toEqual({x: 0, y: 0});
+    expect(state.players[0].patrol.target).toEqual({x: 10, y: 10});
+    expect(state.players[0].patrol.nextTarget).toEqual({x: 0, y: 0});
 });
